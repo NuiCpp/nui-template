30c2a3e97ba9f064dff42638fd3722b7d89f7926
Tim Ebbeke <tim.ebbeke@encoway.de>
2022-10-29 08:00:22 +0200

    Bumped nui version.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        7fb8c4bc9f1b18da1db4f3d3d200a8f581555cd7    
+	GIT_TAG        2b100ef72828f2e5f7fe1bb09864b6a1d018f10c    
 )
 FetchContent_MakeAvailable(nui)
