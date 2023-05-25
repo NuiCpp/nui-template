87d0ebe2cb8c187d6892fa43e5b4d74825670952
Tim Ebbeke <tim06tr@gmail.com>
2023-05-25 05:37:28 +0200

    Bumped nui for linux.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        2034dc742adf6c219b04af4c4dae55724319e4b9    
+	GIT_TAG        c8739f14e00496462e6fbe9397d67288b1cfbb3e    
 )
 FetchContent_MakeAvailable(nui)
