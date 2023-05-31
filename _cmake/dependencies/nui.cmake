bef600ee542d1a9b0faa1555bb0b8d88a0265276
Tim Ebbeke <tim06tr@gmail.com>
2023-05-31 02:06:01 +0200

    Bumped nui to contain stabilize feature.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        64b47ecf9ea522c7d82b6e7b64eb8c3329f27cc0    
+	GIT_TAG        a840a596e6882f2daf443b987f827e15b50431fb    
 )
 FetchContent_MakeAvailable(nui)
