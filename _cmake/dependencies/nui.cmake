c4c81d3aaee5c2b0e033c7c4c04d6af9594ca3a1
Tim Ebbeke <Tim06TR@gmail.com>
2023-08-19 18:30:53 +0200

    Relaxed C++ standard requirement.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        f0ddf68c72a5b0de65aa2d6f25d53c11c89dba36    
+	GIT_TAG        0dc7f2b4ca72cb22a4e98242c18fab0747c9cd3e    
 )
 FetchContent_MakeAvailable(nui)
