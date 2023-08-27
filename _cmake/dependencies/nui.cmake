abea14abccb9c0249a59d9c6b3f40becfadb3c06
Tim Ebbeke <Tim06TR@gmail.com>
2023-08-27 23:56:33 +0200

    Updated template for new nui version.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        0dc7f2b4ca72cb22a4e98242c18fab0747c9cd3e    
+	GIT_TAG        91b75fa48a75927c4094eefd65be543fcbf2255e    
 )
 FetchContent_MakeAvailable(nui)
