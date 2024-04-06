c3dbcfa28089c9dee74f3674303ed13e4510cc52
Tim Ebbeke <Tim06TR@gmail.com>
2024-04-06 16:39:04 +0200

    Updated nui tag.

--- _cmake/dependencies/nui.cmake
+++ _cmake/dependencies/nui.cmake
@@ -2,6 +2,6 @@ include(FetchContent)
 FetchContent_Declare(
 	nui
 	GIT_REPOSITORY https://github.com/NuiCpp/Nui.git
-	GIT_TAG        v0.12.2
+	GIT_TAG        v0.13.1
 )
 FetchContent_MakeAvailable(nui)
